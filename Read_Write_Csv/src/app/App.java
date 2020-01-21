8909dc5d9bd046a159eb45b96c3d966d9d2cbde5
yukiFunasaka <ffffna0lucky@gmail.com>
2020-01-21 23:20:02 +0900

    test commit

--- Read_Write_Csv/src/app/App.java
+++ Read_Write_Csv/src/app/App.java
@@ -1,7 +1,29 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        File csv = new File("sample.csv");
+        InputStreamReader input = new InputStreamReader(new FileInputStream(csv), "UTF-8");
+        BufferedReader br = new BufferedReader(input);
+
+        try {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String arr[] = line.split(",");
+                for(String val : arr){
+                    System.out.println(val);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            br.close();
+        }
     }
 }
